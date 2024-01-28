9f07a5eb7deece23ebe5aa488461abe16bb93b14
SealifeFoam <77304337+SealifeFoam@users.noreply.github.com>
2024-01-28 16:03:47 -0700

    Initial commit

---
+++ W1_ Dish/MadeDish/scrip_dish.js
@@ -0,0 +1,106 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {//Platter
+  background(225);
+  noStroke(); 
+  fill('#a36361');
+  rect(60,170,50,30);
+  rect(100,140,200,90);
+  
+  fill('#a36361');
+  circle(60,185,30);
+  circle(300, 185, 90);
+  
+  //crackers
+  fill('#DCCEB0');
+  rect(110,155,30,40);
+  fill('#C2B79D');
+  rect(110,165,30,40);
+  fill('#DCCEB0');
+  rect(110,175,30,40);
+  fill('#f2e3c2'); 
+  rect(110,185,30,40);
+  
+  //cheese
+  fill('#e2b469')
+  triangle(195, 195, 155, 195, 155, 145);
+  fill('#F8D090')
+  triangle(160, 145, 195, 190, 200, 145);
+  
+  //meat _ change x pts 
+   fill('#B75841');
+  circle(270, 215,25);
+  fill('#c86249');
+  circle(260, 215, 25);
+  fill('#B75841');
+  circle(250, 215,25);
+  fill('#c86249');
+  circle(240, 215, 25);
+  fill('#B75841');
+  circle(230, 215,25);
+  fill('#c86249');
+  circle(220, 215, 25);
+  fill('#B75841');
+  circle(210, 215,25);
+  fill('#c86249');
+  circle(200, 215, 25);
+  fill('#B75841');
+  circle(190, 215,25);
+  fill('#c86249');
+  circle(180, 215, 25);
+  fill('#BA5C44');
+  circle(170, 215,25);
+  fill('#c86249');
+  circle(160, 215,25);
+
+  
+  
+  //bowel
+  fill('#7d919a');
+  circle(300, 180, 60);
+  fill('#5A6A71');
+  circle(300, 180,45);
+  
+  //grapes
+  fill('#829e88');
+  ellipse(290,170,15,20);
+   fill('#7A9580');
+  ellipse(300,170,20,15);
+   fill('#829e88');
+  ellipse(310,170,15,20);
+   fill('#7A9580');
+  ellipse(300,180,20,15);
+   fill('#829e88');
+  ellipse(290,190,15,20);
+   fill('#7A9580');
+  ellipse(310,190,20,15);
+  
+  //veggies
+  fill('#8FA395')
+  circle(220, 155, 20);
+  fill('#b6cdbd')
+  circle(220, 155, 15);
+  fill('#8FA395')
+  circle(220, 165, 20);
+  fill('#b6cdbd')
+  circle(220, 165, 15);
+  fill('#8FA395')
+  circle(220, 175, 20);
+  fill('#b6cdbd')
+  circle(220, 175, 15);
+  fill('#8FA395')
+  circle(220, 185, 20);
+  fill('#b6cdbd')
+  circle(220, 185, 15);
+  
+  fill('#ec9689')//dip
+  arc(240, 150, 60, 60, 0, HALF_PI);
+   fill('#F4D8D4')
+  arc(243, 152, 50, 50, 0, HALF_PI);
+  fill('#')
+  
+} 
+
+//vector flowers??? 
